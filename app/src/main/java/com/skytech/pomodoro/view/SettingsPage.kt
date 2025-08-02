8da7f1799fbfad77de7f0512eb18c2c3c123739b
Melih Çakmak <melihcakmak1755@gmail.com>
2025-08-02 18:50:24 +0300

    refactoering

--- app/src/main/java/com/skytech/pomodoro/view/SettingsPage.kt
+++ app/src/main/java/com/skytech/pomodoro/view/SettingsPage.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -33,27 +34,31 @@ import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingsScreen(preferencesManager: PreferencesManager,closeButton:() -> Unit,viewModel: HomeViewModel){
-    val context= LocalContext.current
+fun SettingsScreen(
+    preferencesManager: PreferencesManager,
+    closeButton: () -> Unit,
+    viewModel: HomeViewModel
+) {
+    val context = LocalContext.current
 
 
-  var focusTime = remember {
-      mutableStateOf(HomeViewModel.PomodoroState.FOCUS.durationInMinutes/60)
-  }
+    var focusTime = remember {
+        mutableStateOf(HomeViewModel.PomodoroState.FOCUS.durationInMinutes / 60)
+    }
     var shortTime = remember {
-        mutableStateOf(HomeViewModel.PomodoroState.SHORT_BREAK.durationInMinutes/60)
+        mutableStateOf(HomeViewModel.PomodoroState.SHORT_BREAK.durationInMinutes / 60)
     }
     var longTime = remember {
-        mutableStateOf(HomeViewModel.PomodoroState.LONG_BREAK.durationInMinutes/60)
+        mutableStateOf(HomeViewModel.PomodoroState.LONG_BREAK.durationInMinutes / 60)
     }
 
-    var soundState= remember {
+    var soundState = remember {
         viewModel.sound
     }
 
-    LaunchedEffect(key1 = true ){
+    LaunchedEffect(key1 = true) {
         CoroutineScope(Dispatchers.Default).launch {
-            viewModel.sound.value=preferencesManager.getSound()
+            viewModel.sound.value = preferencesManager.getSound()
 
         }
     }
@@ -62,36 +67,62 @@ fun SettingsScreen(preferencesManager: PreferencesManager,closeButton:() -> Unit
 
 
 
-    Box(modifier = Modifier
-        .size(height = 300.dp, width = 400.dp)
-        .background(color = Color.White)){
-        
+    Box(
+        modifier = Modifier
+            .size(height = 300.dp, width = 400.dp)
+            .background(color = Color.White)
+    ) {
+
         Column {
-            ListItem(headlineText = { Text(text = "Settings", fontWeight = FontWeight.Bold) }, trailingContent = { IconButton(
-                onClick = closeButton) {
-                Icon(painter = painterResource(id = R.drawable.close_icon), contentDescription = "")
+            ListItem(
+                headlineText = { Text(text = "Settings", fontWeight = FontWeight.Bold) },
+                trailingContent = {
+                    IconButton(
+                        onClick = closeButton
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.close_icon),
+                            contentDescription = ""
+                        )
 
-            }})
+                    }
+                })
 
-           ListItem(headlineText = { Text(text = "Pomodoro Lenght") }, trailingContent = { ChangeValueButton(HomeViewModel.PomodoroState.FOCUS,focusTime
-           ) })
-           ListItem(headlineText = { Text(text = "Short Break Lenght") }, trailingContent = { ChangeValueButton(HomeViewModel.PomodoroState.SHORT_BREAK,shortTime) })
+            ListItem(headlineText = { Text(text = "Pomodoro Lenght") }, trailingContent = {
+                ChangeValueButton(
+                    HomeViewModel.PomodoroState.FOCUS, focusTime
+                )
+            })
+            ListItem(
+                headlineText = { Text(text = "Short Break Lenght") },
+                trailingContent = {
+                    ChangeValueButton(
+                        HomeViewModel.PomodoroState.SHORT_BREAK,
+                        shortTime
+                    )
+                })
 
-           ListItem(headlineText = { Text(text = "Long Break Lenght") }, trailingContent = { ChangeValueButton(HomeViewModel.PomodoroState.LONG_BREAK,longTime) })
+            ListItem(
+                headlineText = { Text(text = "Long Break Lenght") },
+                trailingContent = {
+                    ChangeValueButton(
+                        HomeViewModel.PomodoroState.LONG_BREAK,
+                        longTime
+                    )
+                })
 
-           ListItem(headlineText = { Text(text = "Sound ") }, trailingContent = { Switch(
-               checked = viewModel.sound.value,
-               onCheckedChange = {
-                   viewModel.changeSound()
+            ListItem(headlineText = { Text(text = "Sound ") }, trailingContent = {
+                Switch(
+                    checked = viewModel.sound.value,
+                    onCheckedChange = {
+                        viewModel.changeSound()
 
-                   CoroutineScope(Dispatchers.Default).launch {
-                       preferencesManager.setSound(viewModel.sound.value)
-                   }
-               }
-           )})
-
-
-
+                        CoroutineScope(Dispatchers.Default).launch {
+                            preferencesManager.setSound(viewModel.sound.value)
+                        }
+                    }
+                )
+            })
 
 
         }
